257b5ae56351bab24528a78b2f5f4552ae58c48d
samiha-mahin <samihamahin36@gmail.com>
2025-02-16 01:35:28 +0600

    job-portal

--- frontend/src/components/admin/PostJob.jsx
+++ frontend/src/components/admin/PostJob.jsx
@@ -1,25 +1,115 @@
-import React from 'react'
-import Navbar from '../shared/Navbar'
-import { Label } from '../ui/label'
+import React, { useState } from "react";
+import Navbar from "../shared/Navbar";
+import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 
 const PostJob = () => {
+  const [input, setInput] = useState({
+    tittle: "",
+    description: "",
+    requirements: "",
+    salary: "",
+    experienceLevel: "",
+    location: "",
+    jobType: "",
+    position: 0,
+    companyId: "",
+  });
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
   return (
     <div>
-        <Navbar/>
-        <div className='felx items-center justify-center max-w-6xl mx-auto my-5'>
-            <Label>Tittle</Label>
-            <Input
-            type = "text"
-            name = "title"
-            // value = {}
-            // onChange = {}
-            className = "focus-visible:ring-offset-0 focus-visible:ring-0 my-1"
-            />
-        </div>
+      <Navbar />
+      <div className="felx items-center justify-center max-w-4xl mx-auto my-5">
+          <form action="" className="p-8 max-w-4xl border-gray-200 shadow-lg rounded-md">
+          <div className="grid grid-cols-2 gap-2">
+            <div>
+              <Label>Tittle</Label>
+              <Input
+                type="text"
+                name="tittle"
+                value={input.tittle}
+                onChange={changeEventHandler}
+                className="focus-visible:ring-offset-0 focus-visible:ring-0 my-1"
+              />
+            </div>
+            <div>
+              <Label>Description</Label>
+              <Input
+                type="text"
+                name="description"
+                value={input.description}
+                onChange={changeEventHandler}
+                className="focus-visible:ring-offset-0 focus-visible:ring-0 my-1"
+              />
+            </div>
+            <div>
+              <Label>Requirements</Label>
+              <Input
+                type="text"
+                name="requirements"
+                value={input.requirements}
+                onChange={changeEventHandler}
+                className="focus-visible:ring-offset-0 focus-visible:ring-0 my-1"
+              />
+            </div>
+            <div>
+              <Label>Salary</Label>
+              <Input
+                type="text"
+                name="salary"
+                value={input.salary}
+                onChange={changeEventHandler}
+                className="focus-visible:ring-offset-0 focus-visible:ring-0 my-1"
+              />
+            </div>
+            <div>
+              <Label>Experience Level</Label>
+              <Input
+                type="text"
+                name="experienceLevel"
+                value={input.experienceLevel}
+                onChange={changeEventHandler}
+                className="focus-visible:ring-offset-0 focus-visible:ring-0 my-1"
+              />
+            </div>
+            <div>
+              <Label>Location</Label>
+              <Input
+                type="text"
+                name="location"
+                value={input.location}
+                onChange={changeEventHandler}
+                className="focus-visible:ring-offset-0 focus-visible:ring-0 my-1"
+              />
+            </div>
+            <div>
+              <Label>Job Type</Label>
+              <Input
+                type="text"
+                name="jobType"
+                value={input.jobType}
+                onChange={changeEventHandler}
+                className="focus-visible:ring-offset-0 focus-visible:ring-0 my-1"
+              />
+            </div>
+            <div>
+              <Label>Position</Label>
+              <Input
+                type="number"
+                name="position"
+                value={input.position}
+                onChange={changeEventHandler}
+                className="focus-visible:ring-offset-0 focus-visible:ring-0 my-1"
+              />
+            </div>
+            </div>
+          </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostJob
+export default PostJob;
