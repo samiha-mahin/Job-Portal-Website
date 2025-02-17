2314352e676b44701e0742e8149e03f68ffa607e
samiha-mahin <samihamahin36@gmail.com>
2025-02-18 02:46:08 +0600

    job-portal

--- frontend/src/components/admin/ApplicantsTable.jsx
+++ frontend/src/components/admin/ApplicantsTable.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table';
+
+
+const shortListingStatus = ["Accepted","Rejected"];
 
 const ApplicantsTable = () => {
   return (
-    <div>ApplicantsTable</div>
+    <div>
+        <Table>
+            <TableCaption>A list of your recent applied user</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Fullname</TableHead>
+                    <TableHead>Email</TableHead>
+                    <TableHead>Contact</TableHead>
+                    <TableHead>Resume</TableHead>
+                    <TableHead>Date</TableHead>
+                    <TableHead className="text-right">Action</TableHead>
+                </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <tr>
+                        <TableCell>Full Name</TableCell>
+                        <TableCell>Email</TableCell>
+                        <TableCell>Contact</TableCell>
+                        <TableCell>Resume</TableCell>
+                        <TableCell>Date</TableCell>
+                        <TableCell></TableCell>
+                    </tr>
+                </TableBody>
+        </Table>
+    </div>
   )
 }
 
