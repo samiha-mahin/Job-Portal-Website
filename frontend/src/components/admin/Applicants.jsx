d29369aa54c288ee82d220ca5a80f0c0ed7a9442
samiha-mahin <samihamahin36@gmail.com>
2025-02-18 02:29:04 +0600

    job-portal

--- frontend/src/components/admin/Applicants.jsx
+++ frontend/src/components/admin/Applicants.jsx
@@ -7,7 +7,7 @@ const Applicants = () => {
     <div>
       <Navbar/>
       <div className='max-w-7xl mx-auto'>
-        <h1>Applicants (3)</h1>
+        <h1 className='font-bold text-xl my-5'>Applicants (3)</h1>
         <ApplicantsTable/>
       </div>
     </div>
