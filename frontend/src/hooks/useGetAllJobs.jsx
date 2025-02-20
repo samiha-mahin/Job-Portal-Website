e4a9f1290413275a07b9ef2b6b2c6da92efca5fd
samiha-mahin <samihamahin36@gmail.com>
2025-02-21 01:06:31 +0600

    job-portal

--- frontend/src/hooks/useGetAllJobs.jsx
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -2,14 +2,15 @@ import { setAllJobs } from '@/redux/jobSlice';
 import { JOB_API } from '@/utils/constant';
 import axios from 'axios';
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const useGetAllJobs = () => {
  const dispatch = useDispatch();
+ const {searchedQuery} = useSelector(store=>store.job);
  useEffect(()=>{
     const fecthAllJobs = async()=>{
         try {
-            const res = await axios.get(`${JOB_API}/get`,{withCredentials:true});
+            const res = await axios.get(`${JOB_API}/get?keyword=${searchedQuery}`,{withCredentials:true});
             if(res.data.success){
                 dispatch(setAllJobs(res.data.jobs));
             }
