baf017941ba1ef6ed61973f245ae1f7fdd6f438b
samiha-mahin <samihamahin36@gmail.com>
2025-02-20 22:49:40 +0600

    job-portal

--- frontend/src/hooks/useGetAppliedJob.jsx
+++ frontend/src/hooks/useGetAppliedJob.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { setAllAppliedJobs } from '@/redux/jobSlice';
+import { APPLICATION_API } from '@/utils/constant';
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 const useGetAppliedJob = () => {
-  return (
-    <div>useGetAppliedJob</div>
-  )
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        const fetchAppliedJob = async() =>{
+            try {
+                const res = await axios.get(`${APPLICATION_API}/get`,{withCredentials:true});
+                if(res.data.success){
+                    dispatch(setAllAppliedJobs(res.data.application))
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchAppliedJob();
+    },[])
 }
 
 export default useGetAppliedJob
