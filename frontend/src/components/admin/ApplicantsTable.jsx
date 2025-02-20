29a9d05e86c05d618f57af18f4889f03409b2a26
samiha-mahin <samihamahin36@gmail.com>
2025-02-20 13:40:13 +0600

    job-portal

--- frontend/src/components/admin/ApplicantsTable.jsx
+++ frontend/src/components/admin/ApplicantsTable.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table';
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
 import { MoreHorizontal } from 'lucide-react';
+import { useSelector } from 'react-redux';
 
 
 const shortListingStatus = ["Accepted","Rejected"];
 
 const ApplicantsTable = () => {
+   const {applicants} = useSelector(store=>store.application);
   return (
     <div>
         <Table>
